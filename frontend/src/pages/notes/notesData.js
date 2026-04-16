3a70b5aaf07fee3ffe090833825abc8cc7cb6386
Ank tiwari <premtiwar704@gmail.com>
2026-04-16 23:30:19 +0530

    Clean up notesData.js by removing PDF imports
    Removed unused PDF imports from notesData.js

--- frontend/src/pages/notes/notesData.js
+++ frontend/src/pages/notes/notesData.js
@@ -1,29 +1,9 @@
-// Notes Data - This can be expanded or loaded from an API
-// Note: PDF files should be placed in the public folder or update paths accordingly
-import  iwt1 from'./notes/IWT UNIT-1 Notes.pdf'
-import  iwt2 from'./notes/IWT UNIT-2 Notes.pdf'
-import  iwt3 from'./notes/IWT UNIT-3 Notes.pdf'
-import  iwt4 from'./notes/IWT UNIT-4 Notes.pdf'
-import  iwt5 from'./notes/iwt.pdf'
-
-import sem31 from './notes/environmental_engineering.pdf';
-import sem32 from './notes/discrete_structures.pdf';
-import sem33 from './notes/data_structure.pdf';
-import sem34 from './notes/digital_systems.pdf';
-import sem35 from './notes/oop.pdf';
-                   
-import sem41 from './notes/m-III.pdf';
-import sem42 from './notes/ada.pdf';
-import sem43 from './notes/se.pdf';
-import sem44 from './notes/coa.pdf';
-import sem45 from './notes/os.pdf';
 
 
 
 
 
-import  Toc from'./notes/Toc unit 1,2.pdf'
-import  pr from'./notes/PR unit 1&2.pdf'
+
 export const notesData = [
     { 
         semester: "1", 
